880a66fad99910ef665fac96ba06726c603288cf
QuentinP <quentinprrt33@outlook.fr>
2021-11-23 15:46:37 +0100

    ajout de la fonction definirValeurCase plus une patie des prog d'antoine

--- chasseAuxTresors/Program.cs
+++ chasseAuxTresors/Program.cs
@@ -10,15 +10,48 @@ namespace chasseAuxTresors
     {
         static void Main(string[] args)
         {
-            
+            PlayGame();
         }
-        static int NbLigne = 5;
-        static int NbColonne = 6;
-        static string[,] GrilleAll = new string[NbLigne,NbColonne];
-
-        static void creerBombesTresors(int entreeLigne1 , int entreeColonne1)
+        static string[,] creerGrille()
+        {
+            bool test = false;
+            int nbColonne = 0;
+            int nbLigne = 0;
+            while (test == false)
+            {
+                try
+                {
+                    Console.WriteLine("Combien de lignes voulez-vous ?");
+                    nbLigne = int.Parse(Console.ReadLine());
+                    test = true;
+                }
+                catch (System.FormatException)
+                {
+                    Console.WriteLine("Ceci n'est pas un caractère");
+                }
+            }
+            test = false;
+            while (test == false)
+            {
+                try
+                {
+                    Console.WriteLine("Combien de colonnes voulez-vous ?");
+                    nbColonne = int.Parse(Console.ReadLine());
+                    test = true;
+                }
+                catch (System.FormatException)
+                {
+                    Console.WriteLine("Ceci n'est pas un caractère");
+                }
+            }
+            string[,] grille = new string[nbLigne, nbColonne];
+            return grille;
+        }
+        static void creerBombesTresors(string[,] GrilleAll , int entreeLigne1 , int entreeColonne1)
         {
             Random RdNumber = new Random();
+            int NbLigne = GrilleAll.GetLength(0);
+            int NbColonne = GrilleAll.GetLength(1);
             //Création des trésors 
             int NbTresor = RdNumber.Next(1, 4);
             for (int i = 0; i < NbTresor; i++)
@@ -47,6 +80,38 @@ namespace chasseAuxTresors
                 while (GrilleAll[NumLigne, NumColonne] == "T" || GrilleAll[NumLigne, NumColonne] == "B" || (NumLigne == entreeLigne1 & NumColonne == entreeColonne1));
                 GrilleAll[NumLigne, NumColonne] = "B";
             }
+        }
+        static void definirValeurCase(string[,] GrilleAll , int NumLigne, int NumColonne)
+        {
+            int valeur = 0;
+            try
+            {
+                for(int i = -1; i < 2; i++)
+                {
+                    for(int j  = -1; j< 2; j++)
+                    {
+                        if(GrilleAll[(NumLigne + i),(NumColonne + j)] == "T")
+                        {
+                            valeur += 2;
+                        }
+                        else if (GrilleAll[(NumLigne + i), (NumColonne + j)] == "B")
+                        {
+                            valeur += 1;
+                        }
+                    }
+                }
+                if (valeur != 0)
+                {
+                    GrilleAll[NumLigne, NumColonne] = valeur.ToString();
+                }
+            }
+            catch(System.IndexOutOfRangeException) { }
+        }
+        static void PlayGame()
+        {
+            Console.WriteLine("Bienvenue dans la chasse au trésor !!!");
+            string[,] mainGrille = creerGrille();
+            string[,] calque = mainGrille;
         }
     }
 }
