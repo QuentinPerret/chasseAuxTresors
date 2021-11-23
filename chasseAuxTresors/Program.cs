ffceb05f1381b74d71ffb263365b489e295a10c8
QuentinP <quentinprrt33@outlook.fr>
2021-11-23 14:54:36 +0100

    ajout de reerBombesTresors

--- chasseAuxTresors/Program.cs
+++ chasseAuxTresors/Program.cs
@@ -10,9 +10,43 @@ namespace chasseAuxTresors
     {
         static void Main(string[] args)
         {
-
+            
         }
+        static int NbLigne = 5;
+        static int NbColonne = 6;
+        static string[,] GrilleAll = new string[NbLigne,NbColonne];
 
-
+        static void creerBombesTresors(int entreeLigne1 , int entreeColonne1)
+        {
+            Random RdNumber = new Random();
+            //Création des trésors 
+            int NbTresor = RdNumber.Next(1, 4);
+            for (int i = 0; i < NbTresor; i++)
+            {
+                int NumLigne = 0;
+                int NumColonne = 0;
+                do
+                {
+                    NumLigne = RdNumber.Next(0, NbLigne);
+                    NumColonne = RdNumber.Next(0, NbColonne);
+                }
+                while (GrilleAll[NumLigne, NumColonne] == "T") ;
+                GrilleAll[NumLigne, NumColonne] = "T";
+            }
+            //Création des Bombes
+            int NbBombes = RdNumber.Next((NbLigne / 2), ((NbLigne * NbColonne) / 2 + 1));
+            for (int i = 0; i < NbTresor; i++)
+            {
+                int NumLigne = 0;
+                int NumColonne = 0;
+                do
+                {
+                    NumLigne = RdNumber.Next(0, NbLigne);
+                    NumColonne = RdNumber.Next(0, NbColonne);
+                }
+                while (GrilleAll[NumLigne, NumColonne] == "T" || GrilleAll[NumLigne, NumColonne] == "B" || (NumLigne == entreeLigne1 & NumColonne == entreeColonne1));
+                GrilleAll[NumLigne, NumColonne] = "B";
+            }
+        }
     }
 }
